09cfb787192e002c6babb727b528f0d2e587760d
Omkar2k5 <gondkaromkar53@gmail.com>
2025-07-26 09:00:00 +0530

    docs: add project documentation and results

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cd C:\Users\emmij\OneDrive\cmd\fisat\S6\miniproject\dl-group\code\v3\
+python object_tracker.py --video ./data/video/test.mp4 --output ./outputs/tracker.avi --model yolov4 --info
