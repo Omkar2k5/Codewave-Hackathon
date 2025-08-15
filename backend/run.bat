907b4788bfbb21d3cfc949324ba716cb18c226c7
Omkar2k5 <gondkaromkar53@gmail.com>
2025-08-15 12:29:32 +0530

    Removed and OPtimized Codebase

--- backend/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cd C:\Users\emmij\OneDrive\cmd\fisat\S6\miniproject\dl-group\code\v3\
-python object_tracker.py --video ./data/video/test.mp4 --output ./outputs/tracker.avi --model yolov4 --info
